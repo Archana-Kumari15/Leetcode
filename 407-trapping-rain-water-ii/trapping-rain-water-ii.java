6278f9ed035f073a05e48005d1f5671ff2ec9bb6
Archana-Kumari15 <raoarchana2907@gmail.com>
2025-10-03 20:57:10 +0530

    Time: 17 ms (95.02%) | Memory: 46.7 MB (31.95%) - LeetSync

---
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+     class Cell implements Comparable<Cell> {
+        int r, c, h;
+        Cell(int r, int c, int h) { this.r = r; this.c = c; this.h = h; }
+        public int compareTo(Cell other) { return this.h - other.h; }
+    }
+    public int trapRainWater(int[][] heightMap) {
+                int m = heightMap.length, n = heightMap[0].length;
+        if (m <= 2 || n <= 2) return 0;
+
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>();
+
+        // Add all boundary cells to the PQ
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = visited[i][n - 1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = visited[m - 1][j] = true;
+        }
+
+        int water = 0;
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while (!pq.isEmpty()) {
+            Cell cur = pq.poll();
+            for (int[] d : dirs) {
+                int nr = cur.r + d[0], nc = cur.c + d[1];
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n || visited[nr][nc]) continue;
+                visited[nr][nc] = true;
+
+                int nh = heightMap[nr][nc];
+                if (nh < cur.h) {
+                    water += cur.h - nh;   // trap water
+                    pq.offer(new Cell(nr, nc, cur.h)); // fill up to boundary
+                } else {
+                    pq.offer(new Cell(nr, nc, nh));    // new boundary
+                }
+            }
+        }
+
+        return water;
+    }
+}
