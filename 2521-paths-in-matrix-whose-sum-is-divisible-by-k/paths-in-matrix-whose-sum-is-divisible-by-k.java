affbdd7d462f48eb061946ed7228041f65da39aa
Archana-Kumari15 <raoarchana2907@gmail.com>
2025-11-26 22:32:27 +0530

    Time: 77 ms (51.80%) | Memory: 120.3 MB (11.71%) - LeetSync

---
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfPaths(int[][] grid, int k) {
+       int m = grid.length;
+        int n = grid[0].length;
+        long MOD = 1000000007L;
+
+
+        long[][][] dp = new long[m][n][k];
+        dp[0][0][grid[0][0] % k] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j] % k;
+                if (i == 0 && j == 0) continue;
+
+                for (int r = 0; r < k; r++) {
+                    int newR = (r + val) % k;
+                    if (i > 0)
+                        dp[i][j][newR] = (dp[i][j][newR] + dp[i-1][j][r]) % MOD;
+                    if (j > 0)
+                        dp[i][j][newR] = (dp[i][j][newR] + dp[i][j-1][r]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[m-1][n-1][0];
+    }
+}
