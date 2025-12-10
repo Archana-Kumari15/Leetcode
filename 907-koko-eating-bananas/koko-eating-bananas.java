a93e7825767de1bad8f0a19c2345968535ec3f0a
Archana-Kumari15 <raoarchana2907@gmail.com>
2025-12-11 01:28:26 +0530

    Time: 27 ms (18.12%) | Memory: 47.6 MB (52.14%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,25 +1,79 @@
 class Solution {
-    public boolean canEatAll(int[] piles, int h, int k){
-        int hours = 0;
+    // public static boolean isPossible(int k,int h,int[] piles){
+    //     int pile = 0;
+    //     for(int i =0;i<arr.length;i++){
+    //         if(arr[i] % k == 0) pile += arr[i]/k;
+    //         else pile += arr[i]/k +1;
+    //     }
+    //     if(k > h) return false;
+    //     return true;
+    // }
+      public boolean canEatAll(int[] piles, int h, int k){
+
+        int pile = 0;
         for(int p : piles){
-            hours += Math.ceil((double)p/k);
-            if(hours > h) return false;
+            pile += Math.ceil((double)p/k);
+            // else pile += piles[i]/k +1;
         }
-        return hours <=h;
-    }
+        if(pile > h) return false;
+        return true;}
+    //     int hours = 0;
+    //     for(int p : piles){
+    //         hours += Math.ceil((double)p/k);
+    //         if(hours > h) return false;
+    //     }
+    //     return hours <=h;
+    // }
     public int minEatingSpeed(int[] piles, int h) {
-        int lo = 1, hi = 0;
-        for(int p : piles){
-            hi = Math.max(hi,p);
+         int n = piles.length;
+        int mx = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            mx = Math.max(mx,piles[i]);
         }
-        while(lo < hi){
-            int mid = lo + (hi - lo)/2;
+        int lo = 1 , hi = mx;
+        int ans = 0;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
             if(canEatAll(piles,h,mid)){
-                hi = mid;
-            }else{
-                lo = mid + 1;
-            }
+                ans = mid;
+                hi = mid-1;
+            }else lo = mid+1;
         }
-        return lo;
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int lo = 1, hi = 0;
+    //     for(int p : piles){
+    //         hi = Math.max(hi,p);
+    //     }
+    //     while(lo < hi){
+    //         int mid = lo + (hi - lo)/2;
+    //         if(canEatAll(piles,h,mid)){
+    //             hi = mid;
+    //         }else{
+    //             lo = mid + 1;
+    //         }
+    //     }
+    //     return lo;
     }
 }
