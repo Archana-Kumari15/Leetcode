b7e4c922e373830afac31770c3c67dece26ff030
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-01-26 02:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (13.73%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -7,29 +7,44 @@ class MyQueue {
     }
     
     public void push(int x) {
-        st.push(x);
+        // st.push(x);
+
+        if(st.size() == 0) st.push(x);
+        else{
+            while(st.size()>0){
+                helper.push(st.pop());
+            }
+            st.push(x);
+            while(helper.size() > 0){
+                st.push(helper.pop());
+            }
+        }
     }
     
     public int pop() {
-        while(st.size() > 1){
-            helper.push(st.pop());
-        }
-        int val = st.pop();
-        while(helper.size() > 0){
-            st.push(helper.pop());
-        }
-        return val;
+        // while(st.size() > 1){
+        //     helper.push(st.pop());
+        // }
+        // int val = st.pop();
+        // while(helper.size() > 0){
+        //     st.push(helper.pop());
+        // }
+        // return val;
+
+        return st.pop();
     }
     
     public int peek() {
-        while(st.size() > 1){
-            helper.push(st.pop());
-        }
-        int val = st.peek();
-        while(helper.size() > 0){
-            st.push(helper.pop());
-        }
-        return val;
+        // while(st.size() > 1){
+        //     helper.push(st.pop());
+        // }
+        // int val = st.peek();
+        // while(helper.size() > 0){
+        //     st.push(helper.pop());
+        // }
+        // return val;
+
+        return st.peek();
     }
     
     public boolean empty() {
