020da7856b2277be15afdffc8c40e5126f051691
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-03-06 00:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (85.37%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -3,21 +3,21 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         TreeNode curr = root;
         while(curr != null){
-            if(curr.left != null){
+            if(curr.left != null){ // left h
                 TreeNode pred = curr.left;
                 while(pred.right != null && pred.right != curr) 
                 pred = pred.right;
-                if(pred.right == null){
+                if(pred.right == null){  // link
                     pred.right = curr;
                     curr = curr.left;
                 }
-                else{
+                else{                    // unlink
                     pred.right = null;
                     ans.add(curr.val);
                     curr = curr.right;
                 }
             }
-        else{
+        else{                            // left nhi h
             ans.add(curr.val);
             curr = curr.right;
         }
