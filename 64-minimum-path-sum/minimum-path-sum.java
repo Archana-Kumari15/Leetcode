715f083fed833078a4fc52ee500411eea052b904
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-03-12 09:38:24 +0530

    Time: 4 ms (63.45%) | Memory: 50.1 MB (89.87%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] cost = new int[m][n];
-        for(int i = 0 ; i < m ; i++){
+    public int minPathSum(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int[][] dp = new int[m][n];
+        for(int i = 0; i<m ; i++){
             for(int j = 0; j<n ; j++){
-                if(i == 0 && j == 0){
-                    cost[i][j] = grid[i][j];
-                }else{
-                    int up = (i>0) ? cost[i-1][j] : Integer.MAX_VALUE;
-                    int left = (j>0) ? cost[i][j-1] : Integer.MAX_VALUE;
-                 cost[i][j] = grid[i][j] + Math.min(up,left);
-                 }
+                if(i == 0 && j == 0) dp[i][j] = arr[i][j];
+                else if(i == 0) dp[i][j] = arr[i][j] + dp[i][j-1];
+                else if(j == 0) dp[i][j] = arr[i][j] + dp[i-1][j];
+                else dp[i][j] = arr[i][j] + Math.min(dp[i][j-1] ,dp[i-1][j] );
             }
         }
-        return cost[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
