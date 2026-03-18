dec4893b6e05533d11283ac55d63bf2feaf35bd7
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-03-18 21:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (27.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
-    public int fibo(int n, int[] dp){
-        if(n <= 1) return n;
-        if(dp[n] != 0) return dp[n];
-        dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
-        return dp[n];
-    }
     public int fib(int n) {
+        if(n <= 1) return n;
         int[] dp = new int[n+1];
-        return  fibo(n,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i<=n ; i++){
+            dp[2] = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
     }
 }
