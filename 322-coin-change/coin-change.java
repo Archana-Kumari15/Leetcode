50770c4f3de55d7024f41110d906a2e25d850ccc
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-03-26 14:34:00 +0530

    Time: 26 ms (29.15%) | Memory: 47.3 MB (15.46%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,24 +1,18 @@
 class Solution {
-    public long coincount(int i,int[] coins, int amount,long[][] dp){
-        if(i == -1){
-           if(amount == 0) return 0;
-           else return Integer.MAX_VALUE;
-        }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        long skip = coincount(i-1,coins,amount,dp);
-        if(amount - coins[i] < 0) return dp[i][amount] = skip;
-        long pick = 1 + coincount(i,coins,amount - coins[i],dp);
-        return dp[i][amount] = Math.min(pick,skip);
-    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         long[][] dp = new long[coins.length][amount+1];
         for(int i = 0 ; i<dp.length ; i++){
             for(int j = 0; j<dp[0].length ; j++){
-                dp[i][j] = -1;
+                long skip = (i>0) ? dp[i-1][j] : ((j==0) ? 0 : Integer.MAX_VALUE);
+                if(j - coins[i] < 0) dp[i][j] = skip;
+                else{
+                long pick = 1 + dp[i][j-coins[i]];
+                dp[i][j] = Math.min(pick,skip);
+                }
             }
         }
-        int ans = (int)coincount(n-1,coins,amount,dp);
+        int ans = (int)dp[n-1][amount];
         if(ans == Integer.MAX_VALUE) return -1;
         return ans;
     }
