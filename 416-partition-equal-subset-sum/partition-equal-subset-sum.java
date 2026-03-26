5b9951456a071b2bfc9e38004a5511ceb470178a
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-03-26 14:06:04 +0530

    Time: 86 ms (27.08%) | Memory: 76 MB (13.61%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,20 +1,4 @@
 class Solution {
-    public boolean subset(int i ,int[] arr, int target, int[][] dp){
-        if(i==-1){
-            if(target==0) return true;
-            else return false;
-        }
-        if(dp[i][target] != -1)  return (dp[i][target] == 1);
-        boolean ans = false;
-        boolean skip = subset(i-1,arr,target,dp);
-        if(target-arr[i] < 0) return skip;
-        else{
-            boolean pick = subset(i-1,arr,target-arr[i],dp);
-            ans = skip || pick;
-        }
-        dp[i][target] = (ans) ? 1 : 0;
-        return ans;
-    }
     public boolean canPartition(int[] arr) {
         int sum = 0;
         int n = arr.length;
@@ -24,9 +8,16 @@ class Solution {
         int[][] dp = new int[arr.length][target+1];
         for(int i = 0; i<dp.length;i++){
             for(int j = 0; j<dp[0].length;j++){
-                dp[i][j] = -1;
+                boolean ans = false;
+                boolean skip = (i>0) ? (dp[i-1][j] == 1) : (j == 0);
+                if(j-arr[i] < 0) ans = skip;
+                else{
+                boolean pick = (i>0) ? (dp[i-1][j-arr[i]] == 1) : (j == 0);
+                ans = skip || pick;
+            }
+                dp[i][j] = (ans) ? 1 : 0;
             }
         }
-        return subset(n-1,arr,target,dp);
+        return dp[n-1][target] == 1;
     }
 }
