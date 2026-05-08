7fa00c6717a24ecc616ce7a6fcd45f4647246b7b
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-05-08 13:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (55.01%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -1,21 +1,16 @@
 class Solution {
     static int maxdiff;
-    public void findMax(TreeNode root, TreeNode child){
-        if(root == null || child == null) return;
-        maxdiff = Math.max(maxdiff,Math.abs(root.val - child.val));
-        findMax(root,child.left);
-        findMax(root,child.right);
-    }
-    public void findMaxDiff(TreeNode root){
-        if(root == null) return;
-        findMax(root,root.left);
-        findMax(root,root.right);
-        findMaxDiff(root.left);
-        findMaxDiff(root.right);
+    public int findMaxDiff(TreeNode root, int minV, int maxV){
+        if(root == null){
+            return Math.abs(maxV-minV);
+        }
+        minV = Math.min(minV,root.val);
+        maxV = Math.max(maxV,root.val);
+        int l = findMaxDiff(root.left,minV,maxV);
+        int r = findMaxDiff(root.right,minV,maxV);
+        return Math.max(l,r);
     }
     public int maxAncestorDiff(TreeNode root) {
-        maxdiff = -1;
-        findMaxDiff(root);
-        return maxdiff;
+       return  findMaxDiff(root,root.val,root.val);     
     }
 }
