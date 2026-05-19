0252f601f0d45df35ec55d9bcd86d7e1ccce0594
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-05-20 00:53:28 +0530

    Time: 5 ms (78.73%) | Memory: 46 MB (52.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -9,17 +9,6 @@ class Solution {
          }
 
              return ans == real;
-
-        // String s = Integer.toString(x);
-        // int i = 0 , j = s.length()-1;
-        // while(i <= j){
-        //     if(s.charAt(i) != s.charAt(j)){
-        //         return false;
-        //     }
-        //      i++;
-        //      j--;
-        // }
-        // return true; 
        }
 
     }
