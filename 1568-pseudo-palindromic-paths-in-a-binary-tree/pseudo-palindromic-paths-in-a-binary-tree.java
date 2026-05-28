c590ff9859ed43f464416065e4dea532911903ea
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-05-29 00:00:18 +0530

    Time: 6 ms (80.79%) | Memory: 104 MB (83.84%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,25 +1,18 @@
 class Solution {
     int res = 0;
-    public void solve(TreeNode root,int[] count){
+    public void solve(TreeNode root,int count){
         if(root == null) return;
-        count[root.val]++;
+        count = (count ^ (1 << root.val));
         if(root.left == null && root.right == null){
-            int oddfreq = 0;
-            for(int i = 0; i<=9 ; i++){
-                if(count[i] % 2 != 0){
-                    oddfreq++;
-                }
-            }
-            if(oddfreq <= 1){
-                res++;
-            }
+           if((count & (count-1)) == 0){
+            res++;
+           }
         }
         solve(root.left,count);
         solve(root.right,count);
-        count[root.val]--;
     }
     public int pseudoPalindromicPaths (TreeNode root) {
-        int[] count = new int[10];
+        int count = 0;
         solve(root,count);
         return res;
     }
