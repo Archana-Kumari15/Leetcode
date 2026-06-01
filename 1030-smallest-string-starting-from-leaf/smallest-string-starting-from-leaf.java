9ec449194e4f148df123ea01344f012a382d7811
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-06-01 13:44:13 +0530

    Time: 10 ms (7.99%) | Memory: 47.3 MB (7.40%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -1,19 +1,33 @@
 class Solution {
-    String res = "";
-    public void dfs(TreeNode root, String curr){
-        if(root == null) return;
-        curr = (char)(root.val + 'a') + curr;
-        if(root.left == null && root.right == null){
-            if(res.isEmpty() || curr.compareTo(res) < 0){
-                res = curr;
-            }
-            return;
+    class Pair {
+        TreeNode node;
+        String str;
+
+        Pair(TreeNode node, String str) {
+            this.node = node;
+            this.str = str;
         }
-        dfs(root.left,curr);
-        dfs(root.right,curr);
     }
     public String smallestFromLeaf(TreeNode root) {
-        dfs(root,"");
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root,String.valueOf((char)(root.val + 'a'))));
+        String res = "";
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            TreeNode node = p.node;
+            String curr = p.str;
+            if(node.left == null && node.right == null){
+                if(res.equals("") || curr.compareTo(res)<0){
+                    res = curr;
+                }
+            }
+            if(node.left != null){
+                q.offer(new Pair(node.left,(char)(node.left.val + 'a') + curr));
+            }
+           if(node.right != null){
+                q.offer(new Pair(node.right,(char)(node.right.val + 'a') + curr));
+            }
+        }
         return res;
     }
 }
