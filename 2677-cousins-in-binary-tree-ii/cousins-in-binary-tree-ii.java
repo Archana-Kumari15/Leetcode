b2d23d2b2551e807ddaffc13ec76e1cc231af745
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-06-09 00:53:49 +0530

    Time: 14 ms (98.00%) | Memory: 108.2 MB (90.99%) - LeetSync

--- 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.java
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.java
@@ -3,41 +3,28 @@ class Solution {
         if(root == null) return root;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        List<Integer> levelsum = new ArrayList<>();
+        int levelsum = root.val;
         while(!q.isEmpty()){
             int n = q.size();
-            int currlevelsum = 0;
+            int nextlevelsum = 0;
             for(int i = 0; i<n ; i++){
                 TreeNode curr = q.poll();
-                currlevelsum += curr.val;
-                if(curr.left != null){
-                    q.add(curr.left);
-                }
-                if(curr.right != null){
-                    q.add(curr.right);
-                }
-            }
-            levelsum.add(currlevelsum);
-        }
-        q.add(root);
-        root.val = 0;
-        int level = 1;
-        while(!q.isEmpty()){
-            int n = q.size();
-            for(int i = 0; i<n ; i++){
-                TreeNode curr = q.poll();
+                curr.val = levelsum - curr.val;
+            
                 int siblingsum = curr.left != null ? curr.left.val : 0;
                 siblingsum += curr.right != null ? curr.right.val : 0;
                 if(curr.left != null){
-                    curr.left.val = levelsum.get(level) - siblingsum;
+                    nextlevelsum += curr.left.val;
+                    curr.left.val = siblingsum;
                     q.add(curr.left);
                 }
                 if(curr.right != null){
-                    curr.right.val = levelsum.get(level) - siblingsum;
+                    nextlevelsum += curr.right.val;
+                    curr.right.val = siblingsum;
                     q.add(curr.right);
                 }
             }
-            level++;
+            levelsum = nextlevelsum;
         }
         return root;
 
