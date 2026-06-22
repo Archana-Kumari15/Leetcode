87ca9c6eccffcfa5d02554de2e75478bc129369f
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-06-22 23:37:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (39.54%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode solve(int prestart,int poststart, int preend, int[] preorder, int[] postorder){
+        if(prestart> preend) return null;
+        TreeNode root = new TreeNode(preorder[prestart]);
+        if(prestart == preend) return root;
+        int nextnode = preorder[prestart+1];
+        int j = poststart;
+        while(postorder[j] != nextnode) j++;
+        int num = j - poststart + 1;
+        root.left = solve(prestart+1, poststart, prestart+num,preorder,postorder);
+        root.right = solve(prestart+num+1, j+1,preend, preorder,postorder);
+        return root;
+    }
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        int n = preorder.length;
+        return solve(0,0,n-1,preorder,postorder);
+    }
+}
