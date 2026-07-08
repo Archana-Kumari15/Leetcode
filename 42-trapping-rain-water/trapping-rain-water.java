581e4ec9c970041836d9d52a6587b12790465a9e
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-07-08 18:00:04 +0530

    Time: 1 ms (60.10%) | Memory: 47.7 MB (68.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int[] getLeftMax(int[] height,int n){
-        int[] leftMax = new int[n];
-        leftMax[0] = height[0];
-        for(int i = 1; i<n ; i++){
-            leftMax[i] = Math.max(leftMax[i-1],height[i]);
-        }
-        return leftMax;
-    }
-    public int[] getRightMax(int[] height,int n){
-        int[] rightMax = new int[n];
-        rightMax[n-1] = height[n-1];
-        for(int i = n - 2; i >= 0; i--){
-            rightMax[i] = Math.max(rightMax[i+1],height[i]);
-        }
-        return rightMax;
-    }
     public int trap(int[] height) {
         int n = height.length;
-        int[] leftMax = getLeftMax(height,n);
-        int[] rightMax = getRightMax(height,n);
-        int sum = 0;
-        for(int i = 0; i<n ; i++){
-            int water = Math.min(leftMax[i],rightMax[i]) - height[i];
-            sum += water;
+        int i = 0;
+        int j = n - 1;
+        int maxleft = height[i];
+        int maxright = height[j];
+        // int maxstoredWater = 0;
+        int water = 0;
+        while(i < j){
+            if(maxleft <= maxright){
+                i++;
+            maxleft = Math.max(maxleft,height[i]);
+            water += maxleft - height[i];
+            }
+         else if(maxleft > maxright){
+                j--;
+            maxright = Math.max(maxright,height[j]);
+            water += maxright - height[j];
+            }
         }
-        return sum;
+        
+        return water;
     }
 }
