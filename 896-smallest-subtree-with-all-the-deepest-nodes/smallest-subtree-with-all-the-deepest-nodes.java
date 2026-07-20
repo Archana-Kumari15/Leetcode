afc801cf097df2eb4d05b2a3526741d2388f2f98
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-07-20 12:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (91.73%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    class Pair{
+        int depth;
+        TreeNode node;
+        Pair(int depth,TreeNode node){
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+    public Pair solve(TreeNode root){
+        if(root == null) return new Pair(0,null);
+        Pair left = solve(root.left);
+        Pair right = solve(root.right);
+        if(left.depth == right.depth){
+            return new Pair(left.depth+1,root);
+        }else if(left.depth > right.depth){
+            return new Pair(left.depth+1,left.node);
+        }else{
+            return new Pair(right.depth+1,right.node);
+        }
+    }
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return solve(root).node;
+    }
+}
