1685c046391f5c7d32d0db7cfff0c44119ce4cac
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-02 19:10:07 +0530

    Time: 13 ms (96.25%) | Memory: 43.6 MB (69.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,23 @@
 class Solution {
+    int start = 0;
+    int maxlen = 0;
+    public void helper(String s, int i, int j){
+        while(i >= 0 && j<s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+        }
+        int len = j-i-1;
+        if(len > maxlen){
+            maxlen = len;
+            start = i+1;
+        }
+    }
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 2) return s;
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandFromCenter(s, i, i);
-            int len2 = expandFromCenter(s, i, i + 1);
-
-            int len = Math.max(len1, len2);
-
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+        int n = s.length();
+        for(int i = 0; i<n ; i++){
+            helper(s,i,i);
+            helper(s,i,i+1);
         }
-        return s.substring(start, end + 1);
+        return s.substring(start,start+maxlen);
     }
-
-    public int expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() &&
-               s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
-    }
-    
 }
