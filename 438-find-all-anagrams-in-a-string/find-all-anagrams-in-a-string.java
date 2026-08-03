2576bff4cabab9cd881b082d51ddd5bf99558670
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-03 23:36:38 +0530

    Time: 9 ms (92.40%) | Memory: 47 MB (46.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,36 +1,26 @@
 class Solution {
-    boolean allZero(int[] counter){
-        for(int i : counter){
-            if(i != 0) return false;
+    
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int m = p.length();
+        int n = s.length();
+        if(m > n) return ans;
+        int[] pat = new int[26];
+        int[] window = new int[26];
+        for(int i = 0; i<m ; i++){
+            pat[p.charAt(i) - 'a']++;
+            window[s.charAt(i) - 'a']++;
         }
-        return true;
-    }
-    public List<Integer> findAnagrams(String txt, String pat) {
-       int n = txt.length();
-       int[] counter = new int[26];
-       for(int i = 0; i<pat.length() ; i++){
-           char ch = pat.charAt(i);
-           counter[ch - 'a']++;
-       }
-       int i = 0, j = 0;
-       int res = 0;
-       int k = pat.length();
-       List<Integer> ans = new ArrayList<>();
-       while(j < n){
-           counter[txt.charAt(j) - 'a']--;
-           if(j-i+1 == k){
-               if(allZero(counter)){
-                   ans.add(i);
-               }
-               counter[txt.charAt(i) - 'a']++;
-               i++;
-           }
-           j++;
-       }
+        if(Arrays.equals(pat,window)){
+             ans.add(0);
+        }
+        for(int i = m ; i<n ; i++){
+            window[s.charAt(i) - 'a']++;
+            window[s.charAt(i-m) - 'a']--;
+            if(Arrays.equals(pat,window)){
+                ans.add(i-m+1);
+            }
+        }
         return ans;
     }
 }
-
- 
- 
-      
