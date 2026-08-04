3f006bdbfe5b191257888b379bb41f0bad1eae8b
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-04 18:33:21 +0530

    Time: 54 ms (42.50%) | Memory: 116.2 MB (82.81%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int[] canSeePersonsCount(int[] heights) {
+    public int[] canSeePersonsCount(int[] height) {
+        int n = height.length;
+        int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
-        int n = heights.length;
-        int[] res = new int[n];
-        st.push(heights[n-1]);
-        res[n-1] = 0;
-        
-        for(int i = n-2 ; i>=0 ; i--){
-            int count = 0;
-            while(st.size()>0 && st.peek() < heights[i]){
-                st.pop();
-                count++;   
-            }
-            if (st.size()>0) count++;
-            res[i] = count;
-            st.push(heights[i]);
+        for(int i = n-1; i >= 0; i--){
+            int visible = 0;
+           while(!st.isEmpty() && height[i] > st.peek()){
+            st.pop();
+            visible++;
+            
+           }
+           if(!st.isEmpty()) visible++;
+          ans[i] = visible;
+           st.push(height[i]);
         }
-        return res;
+        return ans;
     }
 }
