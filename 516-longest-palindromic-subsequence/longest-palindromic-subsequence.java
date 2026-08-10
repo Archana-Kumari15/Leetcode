f1d47e1ffcb4212799fa93fc9e4ce94938120ad5
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-10 16:37:58 +0530

    Time: 64 ms (15.15%) | Memory: 63.5 MB (77.81%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,32 +1,33 @@
 class Solution {
-    public int lcs(int i, int j , String a, String b,int[][] dp){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)) return dp[i][j] =  1 + lcs(i-1,j-1,a,b,dp);
-        else{
-            return dp[i][j] =  Math.max(lcs(i-1,j,a,b,dp), lcs(i,j-1,a,b,dp));
-        }
-    }
-    public int lcs2(String a, String b){
-        StringBuilder sa = new StringBuilder(a);
-        StringBuilder sb = new StringBuilder(b);
-        int m = sa.length();
-        int n = sb.length();
-        int[][] dp = new int[m][n];
-        for(int i = 0 ; i<dp.length; i++){
-            for(int j = 0; j<dp[0].length; j++){
-                dp[i][j] = -1;
+    public int lcs(String a, String b){
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i<m ; i++){
+            for(int j = 0; j<n ; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
             }
         }
-        return lcs(m-1,n-1,a,b,dp);
+        for(int i = 1; i<m+1 ; i++){
+            for(int j = 1; j<n+1; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
     public String reverse(String s){
         StringBuilder sb = new StringBuilder(s);
-        sb.reverse();
-        s = sb.toString();
-        return s;  
-        }
+        
+        s = sb.reverse().toString();
+        return s;
+    }
     public int longestPalindromeSubseq(String s) {
-        return lcs2(s,reverse(s));
+        return lcs(s,reverse(s));
     }
 }
