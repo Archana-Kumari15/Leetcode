f7c91180fc31367c9f32b5143df795d335e6628e
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-16 21:54:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (83.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,12 +3,10 @@ class Solution {
         int n = s.length;
         if(n == 0) return "";
         String ans = s[0];
-        for(int i = 0; i<ans.length(); i++){
-            char ch = ans.charAt(i);
-            for(int j = 1; j<n ; j++){
-                if(i >= s[j].length() || s[j].charAt(i) != ch){
-                    return ans.substring(0,i);
-                }
+        for(int i = 1; i<n; i++){
+            while(!s[i].startsWith(ans)){
+                ans = ans.substring(0,ans.length()-1);
+                if(ans.length() == 0) return ""; 
             }
         }
         return ans;
