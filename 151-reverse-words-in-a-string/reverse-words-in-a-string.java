467060dd315e721c84deef5dd7fdd12edf1f7039
Archana-Kumari15 <raoarchana2907@gmail.com>
2026-08-18 01:18:14 +0530

    Time: 6 ms (80.56%) | Memory: 44.3 MB (69.72%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,33 +1,26 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] arr = s.toCharArray();
+        List<String> arr = new ArrayList<>();
         int i = 0;
-        int n = arr.length;
-        int write = 0;
+        int n = s.length();
         while(i < n){
-            while(i < n && arr[i] == ' ') i++;
-            while(i < n && arr[i] != ' ') arr[write++] = arr[i++];
-            while(i < n && arr[i] == ' ') i++;
-            if(i < n) arr[write++] = ' ';
-        }
-        reverse(arr,0,write-1);
-        int start = 0;
-        for(int j = 0; j <= write; j++){
-            if(j == write || arr[j] == ' '){
-                reverse(arr,start,j-1);
-                start = j+1;
+            while(i <n && s.charAt(i) == ' ') i++;
+            int start = i;
+            while(i < n && s.charAt(i) != ' ') i++;
+            if(start < i){
+                arr.add(s.substring(start,i));
             }
         }
-        return new String(arr,0,write);
-    }
-
-    public void reverse(char[] arr,int start, int end){
-        while(start < end){
-            char temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+        int l = 0;
+        int r = arr.size()-1;
+        while(l < r){
+            String temp = arr.get(l);
+            arr.set(l,arr.get(r));
+            arr.set(r ,temp);
+            l++;
+            r--;
         }
+        
+        return String.join(" " ,arr);
     }
 }
